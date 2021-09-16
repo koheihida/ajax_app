ff379cd75502e9073a3802bce943fdcbbb0275e0
koheihida <88849254+koheihida@users.noreply.github.com>
2021-09-16 11:52:28 +0900

    メモ投稿機能

---
+++ app/javascript/memo.js
@@ -0,0 +1,15 @@
+function post (){
+  
+  const form = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+ 
+ window.addEventListener('load', post);
+ 
